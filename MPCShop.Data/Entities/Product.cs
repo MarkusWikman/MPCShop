9afcc35da35b3f94b91021fbfb1178a4988bb09d
Christos Boukas <Christosboukast@gmail.com>
2024-01-11 10:18:21 +0100

    added DBContext

--- MPCShop.Data/Entities/Product.cs
+++ MPCShop.Data/Entities/Product.cs
@@ -4,6 +4,7 @@ public class Product : IEntity
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public List<Category>? Categories { get; set; }
     public List<Size>? Sizes { get; set; }
 
 
