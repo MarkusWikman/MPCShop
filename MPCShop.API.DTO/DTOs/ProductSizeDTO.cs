1963667ba361e232421a46904e296cc2bc931b95
ChrisLaptop <ChristosBoukasT@gmail.com>
2024-01-29 10:15:59 +0100

    added SizeDTO and ProductSizeDTO

---
+++ MPCShop.API.DTO/DTOs/ProductSizeDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace MPCShop.API.DTO.DTOs;
+public class ProductSizeDTO
+{
+    public int SizeId { get; set; }
+    public int ProductId { get; set; }
+}
+//public class ProductSizeDeleteDTO : ProductSizePostDTO
+//{
+//}
+//public class ProductSizeGetDTO : ProductSizePostDTO
+//{
+//}
+
+//public class ProductSizeSmallGetDTO
+//{
+//    public int SizeId { get; set; }
+//}
---
+++ MPCShop.API.DTO/DTOs/SizeDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MPCShop.API.DTO.DTOs;
+public class SizePostDTO
+{
+    public string Name { get; set; } = string.Empty;
+}
+public class SizePutDTO : SizePostDTO
+{
+    public int Id { get; set; }
+}
+public class SizeGetDTO : SizePutDTO
+{
+}
