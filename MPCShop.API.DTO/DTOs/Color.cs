363564b52de480ff5afe349637a3efe258ea188d
parisa.azizi8209@gmail.com <parisa.azizi8209@gmail.com>
2024-01-29 10:29:50 +0100

    Color.DTO

---
+++ MPCShop.API.DTO/DTOs/Color.cs
@@ -0,0 +1,13 @@
+﻿namespace MPCShop.API.DTO.DTOs;
+public class ColorPostDTO
+{
+    public string Name { get; set; } = string.Empty;
+}
+public class ColorPutDTO : ProductPostDTO
+{
+    public int Id { get; set; }
+}
+public class ColorGetDTO : ProductPutDTO
+{
+    //public List<FilterGetDTO>? Filters { get; set; }
+}
--- MPCShop.API.OtherEntities/Program.cs
+++ MPCShop.API.OtherEntities/Program.cs
@@ -4,6 +4,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// SQL Server Service Registration
+builder.Services.AddDbContext<MPCShopContext>(
+    options =>
+        options.UseSqlServer(
+            builder.Configuration.GetConnectionString("MPCShopConnection")));
+/**********
+ ** CORS Cross-Origin Resource Sharing**
+ **********/
+builder.Services.AddCors(policy =>
+{
+    policy.AddPolicy("CorsAllAccessPolicy", opt =>
+        opt.AllowAnyOrigin()
+           .AllowAnyHeader()
+           .AllowAnyMethod()
+    );
+});
 
 var app = builder.Build();
 
@@ -16,29 +32,53 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+RegisterEndpoints(); //To register Category
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+/************************
+ ** CORS Configuration **
+ ************************/
+app.UseCors("CorsAllAccessPolicy");
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+
+void RegisterEndpoints()
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    app.AddEndpoint<ProductCategory, ProductCategoryDTO>();
+    //app.AddEndpoint<Category, CategoryPostDTO, CategoryPutDTO, CategoryGetDTO>(); //can be for size color season ...
+    /*app.MapGet($"/api/categorieswithdata", async (IDbService db) =>
+    {
+        try
+        {
+            return Results.Ok(await ((CategoryDbService)db).GetCategoriesWithAllRelatedDataAsync());
+        }
+        catch
+        {
+        }
+
+        return Results.BadRequest($"Couldn't get the requested products of type {typeof(Product).Name}.");
+    });*/
+}
+
+void RegisterServices()
+{
+    ConfigureAutoMapper();
+    builder.Services.AddScoped<IDbService, CategoryDbService>();
+}
+
+void ConfigureAutoMapper()
+{
+    var config = new MapperConfiguration(cfg =>
+    {
+        cfg.CreateMap<Category, CategoryPostDTO>().ReverseMap();
+        cfg.CreateMap<Category, CategoryPutDTO>().ReverseMap();
+        cfg.CreateMap<Category, CategoryGetDTO>().ReverseMap();
+        cfg.CreateMap<Category, CategorySmallGetDTO>().ReverseMap();
+        cfg.CreateMap<ProductCategory, ProductCategoryDTO>().ReverseMap();
+        //cfg.CreateMap<Filter, FilterGetDTO>().ReverseMap();
+        //cfg.CreateMap<Size, OptionDTO>().ReverseMap();
+        //cfg.CreateMap<Color, OptionDTO>().ReverseMap();
+    });
+    var mapper = config.CreateMapper();
+    builder.Services.AddSingleton(mapper);
 }
