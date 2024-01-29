d08c044b43a4b4450b6a9a18c80376aeb28703fa
parisa.azizi8209@gmail.com <parisa.azizi8209@gmail.com>
2024-01-29 09:51:41 +0100

    otherentities

---
+++ MPCShop.API.OtherEntities/using.cs
@@ -0,0 +1,9 @@
+﻿global using MPCShop.Data.Contexts;
+global using Microsoft.EntityFrameworkCore;
+global using MPCShop.API.DTO.DTOs;
+global using MPCShop.API.DTO;
+global using MPCShop.API.Extensions.Extensions;
+global using MPCShop.Data.Entities;
+global using AutoMapper;
+global using MPCShop.Data.Services;
+
